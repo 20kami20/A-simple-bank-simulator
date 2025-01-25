3163b35209652f565f5080afa60a3666af4b0475
BAhdat11 <ayaganbagdat1@icloud.com>
2025-01-25 12:35:51 +0500

    Update Main

---
+++ src/models/User.java
@@ -0,0 +1,59 @@
+package models;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String pinCode;
+    private double balance;
+
+
+    public User(int id, String username, String password, String pinCode, double balance) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.pinCode = pinCode;
+        this.balance = balance;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
